0fef2ea84b1dcd988f45a6b59c1dfb90c817abd2
slelaron <slelaron@gmail.com>
2016-11-07 20:24:40 +0300

    corrections

--- Calc/app/src/main/java/ru/ifmo/android_2016/calc/CalculatorActivity.java
+++ Calc/app/src/main/java/ru/ifmo/android_2016/calc/CalculatorActivity.java
@@ -3,15 +3,162 @@ package ru.ifmo.android_2016.calc;
 import android.app.Activity;
 import android.os.Bundle;
 
+import android.util.Pair;
+import android.view.View;
+import android.widget.TextView;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
 /**
  * Created by alexey.nikitin on 13.09.16.
  */
 
-public final class CalculatorActivity extends Activity {
+public class CalculatorActivity extends Activity {
+
+    private StringBuilder strBuilder = new StringBuilder();
+    private Parser parser;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_calculator);
+        parser = new Parser((TextView)findViewById(R.id.Debug));
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedState) {
+        super.onRestoreInstanceState(savedState);
+        TextView expressionText = (TextView)findViewById(R.id.ExpressionText);
+        expressionText.setText(savedState.getString("Expression text"));
+        if (expressionText.length() != 0) {
+            parser.toRestoreInitialState(savedState.getIntegerArrayList("Coded expression"), new BigDecimal(savedState.getString("Last result")));
+        }
+        strBuilder.append(savedState.getString("Expression text"));
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle saveInstanceState) {
+        TextView expressionText = (TextView)findViewById(R.id.ExpressionText);
+        saveInstanceState.putString("Expression text", expressionText.getText().toString());
+        Pair<ArrayList<Integer>, BigDecimal> tmp = parser.toSaveExp();
+        saveInstanceState.putIntegerArrayList("Coded expression", tmp.first);
+        if (expressionText.getText().toString().length() != 0) {
+            saveInstanceState.putString("Last result", tmp.second.toString());
+        }
+        super.onSaveInstanceState(saveInstanceState);
+    }
+
+
+    public void ClearAll(View view) {
+        parser.clear();
+        TextView text = (TextView) findViewById(R.id.ExpressionText);
+        strBuilder.setLength(0);
+        text.setText("");
+    }
+
+    public void AddSymbol(View view) {
+        TextView text = (TextView)findViewById(R.id.ExpressionText);
+
+        switch(view.getId()) {
+            case R.id.LeftParenthesis:
+                parser.add(18);
+                strBuilder.append('(');
+                break;
+            case R.id.RightParenthesis:
+                parser.add(19);
+                strBuilder.append(')');
+                break;
+            case R.id.Add:
+                parser.add(11);
+                strBuilder.append('+');
+                break;
+            case R.id.Subtract:
+                parser.add(12);
+                strBuilder.append('-');
+                break;
+            case R.id.Modulo:
+                parser.add(15);
+                strBuilder.append('%');
+                break;
+            case R.id.Divide:
+                parser.add(14);
+                strBuilder.append('/');
+                break;
+            case R.id.Multiply:
+                parser.add(13);
+                strBuilder.append('*');
+                break;
+            case R.id.Zero:
+                parser.add(0);
+                strBuilder.append('0');
+                break;
+            case R.id.One:
+                parser.add(1);
+                strBuilder.append('1');
+                break;
+            case R.id.Two:
+                parser.add(2);
+                strBuilder.append('2');
+                break;
+            case R.id.Three:
+                parser.add(3);
+                strBuilder.append('3');
+                break;
+            case R.id.Four:
+                parser.add(4);
+                strBuilder.append('4');
+                break;
+            case R.id.Five:
+                parser.add(5);
+                strBuilder.append('5');
+                break;
+            case R.id.Six:
+                parser.add(6);
+                strBuilder.append('6');
+                break;
+            case R.id.Seven:
+                parser.add(7);
+                strBuilder.append('7');
+                break;
+            case R.id.Eight:
+                parser.add(8);
+                strBuilder.append('8');
+                break;
+            case R.id.Nine:
+                parser.add(9);
+                strBuilder.append('9');
+                break;
+            case R.id.Point:
+                parser.add(17);
+                strBuilder.append('.');
+                break;
+        }
+        text.setText(strBuilder.toString());
+    }
+
+    public void Evaluate(View view) {
+        TextView text = (TextView)findViewById(R.id.ExpressionText);
+        boolean s = parser.checkOnCorrectness();
+        if (s) {
+            BigDecimal res = parser.calculate();
+            text.setText(res.toPlainString());
+            strBuilder.setLength(0);
+            strBuilder.append(res.toPlainString());
+            parser.clear();
+            parser.addNumber(res);
+        }
+    }
+
+
+
+    public void ClearLast(View view) {
+        if (strBuilder.length() == 0) {
+            return;
+        }
+        TextView text = (TextView)findViewById(R.id.ExpressionText);
+        strBuilder.deleteCharAt(strBuilder.length() - 1);
+        text.setText(strBuilder.toString());
+        parser.clearLast();
     }
 }
+
---
+++ Calc/app/src/main/java/ru/ifmo/android_2016/calc/Parser.java
@@ -0,0 +1,215 @@
+package ru.ifmo.android_2016.calc;
+
+import android.util.Pair;
+import android.widget.TextView;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+
+/**
+ * Created by nikita on 27.09.16.
+ */
+
+public class Parser {
+	TextView text;
+	ArrayList <Integer> exp;
+	int pointer;
+	boolean calculatedResult;
+	BigDecimal result;
+	private boolean checkOnNumber() {
+		return (exp.get(pointer) >= 0 && exp.get(pointer) <= 9);
+	}
+	private BigDecimal getNumber() throws Exception {
+		StringBuilder builder = new StringBuilder();
+		boolean was = false;
+		while (exp.get(pointer) >= 0 && exp.get(pointer) <= 9 || exp.get(pointer) == 17) {
+			if (exp.get(pointer) == 17 && !was) {
+				builder.append('.');
+				was = true;
+			} else if (exp.get(pointer) == 17 && was) {
+				throw new Exception("Wrong sequence of characters");
+			} else {
+				builder.append(exp.get(pointer));
+			}
+			pointer++;
+		}
+		return new BigDecimal(builder.toString());
+	}
+	private BigDecimal forthAct() throws Exception {
+		if (exp.get(pointer) == 12) {
+			pointer++;
+			return forthAct().negate();
+		}
+		if (checkOnNumber()) {
+			return getNumber();
+		}
+		if (exp.get(pointer) == 18) {
+			pointer++;
+			BigDecimal result = firstAct();
+			if (exp.get(pointer) == -1) {
+				return result;
+			} else if (exp.get(pointer) != 19) {
+				throw new Exception("No right parenthesis");
+			} else {
+				pointer++;
+			}
+			return result;
+		}
+		throw new Exception("No number in the last position");
+	}
+	private BigDecimal thirdAct() throws Exception {
+		if (exp.get(pointer) == 16) {
+			pointer++;
+			BigDecimal res = firstAct();
+			if (res.compareTo(BigDecimal.ZERO) >= 0) {
+				BigDecimal l = new BigDecimal(0);
+				BigDecimal acc = new BigDecimal(0.00000001);
+				BigDecimal r = res;
+				BigDecimal two = new BigDecimal(2);
+				while (l.subtract(r).compareTo(acc) > 0) {
+					BigDecimal m = l.add(r).divide(two, 9);
+					if (m.multiply(m).compareTo(res) > 0) {
+						r = m;
+					} else {
+						l = m;
+					}
+				}
+				return l;
+			} else {
+				throw new Exception("Negative argument");
+			}
+		}
+		return forthAct();
+	}
+	private BigDecimal secondAct() throws Exception {
+		BigDecimal that = thirdAct();
+		while (true) {
+			if (exp.get(pointer) == 13) {
+				pointer++;
+				that = that.multiply(thirdAct());
+			} else if (exp.get(pointer) == 14) {
+				pointer++;
+				BigDecimal res = thirdAct();
+				if (res.equals(BigDecimal.ZERO)) {
+					throw new Exception("Division by zero");
+				} else {
+					that = that.divide(res, 5, BigDecimal.ROUND_HALF_UP);
+				}
+			} else if (exp.get(pointer) == 15) {
+				pointer++;
+				BigDecimal res = thirdAct();
+				if (res.equals(BigDecimal.ZERO)) {
+					throw new Exception("Division by zero");
+				}
+				that = that.subtract(that.divideToIntegralValue(res).multiply(res));
+			} else {
+				return that;
+			}
+		}
+	}
+	private BigDecimal firstAct() throws Exception {
+		BigDecimal that = secondAct();
+		while (true) {
+			if (exp.get(pointer) == 11) {
+				pointer++;
+				that = that.add(secondAct());
+			} else if (exp.get(pointer) == 12) {
+				pointer++;
+				that = that.subtract(secondAct());
+			} else {
+				return that;
+			}
+		}
+	}
+
+	private void Print() {
+		checkOnCorrectness();
+		if (exp.size() == 1) {
+			text.setText("");
+		} else {
+			text.setText(result.toPlainString());
+		}
+	}
+
+	Parser(TextView tmp) {
+		exp = new ArrayList<>();
+		pointer = 0;
+		exp.add(-1);
+		calculatedResult = false;
+		text = tmp;
+	}
+	public void add(int operation) {
+		exp.set(exp.size() - 1, operation);
+		exp.add(-1);
+		calculatedResult = false;
+		Print();
+	}
+
+	public void addNumber(BigDecimal tmp) {
+		exp.clear();
+		String s = tmp.toPlainString();
+		for (int i = 0; i < s.length(); i++) {
+			if (s.charAt(i) >= '0' && s.charAt(i) <= '9') {
+				exp.add(s.charAt(i) - '0');
+			}
+			if (s.charAt(i) == '.') {
+				exp.add(17);
+			}
+		}
+		exp.add(-1);
+		Print();
+	}
+
+	public void clearLast() {
+		if (exp.size() > 1) {
+			exp.set(exp.size() - 2, -1);
+			exp.remove(exp.size() - 1);
+			calculatedResult = false;
+			Print();
+		}
+	}
+
+	public void clear() {
+		exp.clear();
+		exp.add(-1);
+		result = BigDecimal.ZERO;
+		calculatedResult = false;
+		pointer = 0;
+		Print();
+	}
+
+	public boolean checkOnCorrectness() {
+		if (exp.size() == 1) {
+			result = BigDecimal.ZERO;
+			calculatedResult = false;
+			return false;
+		}
+		if (calculatedResult) {
+			return true;
+		}
+		try {
+			pointer = 0;
+			result = firstAct();
+			calculatedResult = true;
+			return true;
+		} catch (Exception exp) {
+			text.setText(exp.getMessage());
+			return false;
+		}
+	}
+
+	public Pair <ArrayList<Integer>, BigDecimal> toSaveExp() {
+		return new Pair <>(exp, result);
+	}
+
+	public void toRestoreInitialState(ArrayList <Integer> tmp, BigDecimal lastResult) {
+		exp = tmp;
+		result = lastResult;
+		Print();
+	}
+
+	public BigDecimal calculate() {
+		return result;
+	}
+}
